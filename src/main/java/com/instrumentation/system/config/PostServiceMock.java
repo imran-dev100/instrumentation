7f66c3cf3c3530bd323087f3123382fc622d6fa0
imran-dev100 <iagro7189@gmail.com>
2023-10-20 20:35:41 +0530

    Making desirable changes with added comments

--- src/main/java/com/instrumentation/system/config/PostServiceMock.java
+++ src/main/java/com/instrumentation/system/config/PostServiceMock.java
@@ -1,38 +1,58 @@
 package com.instrumentation.system.config;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.instrumentation.system.dto.PostResponse;
 import com.instrumentation.system.entity.Post;
-import com.instrumentation.system.service.PostService;
-import com.instrumentation.system.service.PostServiceImpl;
 
+@SuppressWarnings("unchecked")
 @Profile("REPLAY")
 @Configuration
 public class PostServiceMock {
 
-    @Bean
-    @Primary
-    PostService mockCreateNewPost() {
-		PostService postService = Mockito.mock(PostServiceImpl.class);
-		when(postService.createNewPost(any())).thenReturn(generateMockPostResponse());
-		return postService;
+	@Value("${external.api}")
+	private String externalApi;
+
+	/**
+	 * @return mock of JpaRepository of Post class in REPLAY mode only
+	 */
+	@Bean
+	@Primary
+	@Profile("REPLAY")
+	JpaRepository<Post, Long> mockCreateNewPost() {
+		JpaRepository<Post, Long> postRepository = Mockito.mock(JpaRepository.class);
+		when(postRepository.save(any(Post.class))).thenReturn(generateMockPost());
+		return postRepository;
 	}
 
-	private PostResponse generateMockPostResponse() {
-		return PostResponse.builder().dbRow(generateMockPost()).httpOutbound(generateMockHttpOutbound()).build();
+	/**
+	 * @return mock of RestTemplate for REPLAY mode only
+	 */
+	@Bean
+	@Profile("REPLAY")
+	RestTemplate mockRestTemplate() {
+		RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+		//Mocking external API
+		when(restTemplate.getForEntity(eq(externalApi), any(Class.class)))
+				.thenReturn(new ResponseEntity<>(generateMockHttpOutbound(), HttpStatus.OK));
+		return restTemplate;
 	}
 
 	private Post generateMockPost() {
@@ -40,7 +60,8 @@ public class PostServiceMock {
 	}
 
 	private JsonNode generateMockHttpOutbound() {
-		Map<String, Object> httpOutboundMap = new HashMap<>();
+		//Using LinkedHashMap for maintaining insertion order
+		Map<String, Object> httpOutboundMap = new LinkedHashMap<>();
 		httpOutboundMap.put("abbreviation", "IST");
 		httpOutboundMap.put("client_ip", "49.36.99.42");
 		httpOutboundMap.put("datetime", "2023-10-20T13:11:32.879911+05:30");
--- src/main/java/com/instrumentation/system/service/PostServiceImpl.java
+++ src/main/java/com/instrumentation/system/service/PostServiceImpl.java
@@ -3,6 +3,7 @@ package com.instrumentation.system.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.instrumentation.system.dto.PostResponse;
 import com.instrumentation.system.entity.Post;
-import com.instrumentation.system.repository.PostRepository;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -27,14 +28,19 @@ public class PostServiceImpl implements PostService {
 	private RestTemplate restTemplate;
 
 	@Autowired
-	private PostRepository postRepository;
+	private JpaRepository<Post, Long> postRepository;
 
 	@Override
 	public PostResponse createNewPost(Post post) {
 		ResponseEntity<JsonNode> responseEntity = null;
 		try {
+			//Saving to database
 			post = postRepository.save(post);
+			
+			//Calling an external HTTP API
 			responseEntity = restTemplate.getForEntity(externalApi, JsonNode.class);
+			
+			//Checking if status is 200 OK and response has body
 			if (responseEntity.getStatusCode().equals(HttpStatus.OK) && responseEntity.hasBody())
 				return PostResponse.builder().dbRow(post).httpOutbound(responseEntity.getBody()).build();
 		} catch (DataAccessException de) {
