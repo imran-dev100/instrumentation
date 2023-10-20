297ef8254a8fe021ae567ef7c8c69447d5e543e9
imran-dev100 <iagro7189@gmail.com>
2023-10-20 13:45:23 +0530

    Creating Post entity

---
+++ src/main/java/com/instrumentation/system/entity/Post.java
@@ -0,0 +1,32 @@
+package com.instrumentation.system.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@JsonProperty("post_name")
+	private String name;
+
+	@JsonProperty("post_contents")
+	private String contents;
+}
