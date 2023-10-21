53b9cb822b2e9ae38d4703686a8d999ed74b677a
imran-dev100 <iagro7189@gmail.com>
2023-10-22 00:44:45 +0530

    Disabling PostRepository for REPLAY profile

--- src/main/java/com/instrumentation/system/repository/PostRepository.java
+++ src/main/java/com/instrumentation/system/repository/PostRepository.java
@@ -1,11 +1,13 @@
 package com.instrumentation.system.repository;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.instrumentation.system.entity.Post;
 
+//Enabling Repository for NON-REPLAY mode 
+@Profile("!REPLAY")
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
 }
